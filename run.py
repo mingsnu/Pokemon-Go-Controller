8e4b517f653d5e0c193804c25bfd6475e5f698b9
Weicheng-mac <weich@iastate.edu>
2016-07-14 08:32:50 -0500

    update run,py

--- run.py
+++ run.py
@@ -9,30 +9,32 @@ lastLng = ""
 
 def clickAction():
     os.system("./autoClicker -x 550 -y 880")
+    time.sleep(1)
     os.system("./autoClicker -x 570 -y 600")
-        time.sleep(1)
-        print "clicking!!"
+    time.sleep(1)
+    print "clicking!!"
 
 def getPokemonLocation():
     try:
-        response = urllib2.urlopen("http://192.168.2.3/", timeout = 100)
-            return json.load(response)
-        except urllib2.URLError as e:
-            print e.reason
+        #response = urllib2.urlopen("http://192.168.2.3/", timeout = 100)
+        response = urllib2.urlopen("http://10.26.26.52/", timeout = 100)
+        return json.load(response)
+    except urllib2.URLError as e:
+        print e.reason
 
 def generateXML():
     global lastLat, lastLng
-        geo = getPokemonLocation()
-        if geo != None:
-            if geo["lat"] != lastLat or geo["lng"] != lastLng:
-                lastLat = geo["lat"]
-                    lastLng = geo["lng"]
-                        gpx = ET.Element("gpx", version="1.1", creator="Xcode")
-                        wpt = ET.SubElement(gpx, "wpt", lat=geo["lat"], lon=geo["lng"])
-                        ET.SubElement(wpt, "name").text = "PokemonLocation"
-                        ET.ElementTree(gpx).write("pokemonLocation.gpx")
-                        print "Location Updated!", "latitude:", geo["lat"], "longitude:" ,geo["lng"]
-                        clickAction()
+    geo = getPokemonLocation()
+    if geo != None:
+        if geo["lat"] != lastLat or geo["lng"] != lastLng:
+            lastLat = geo["lat"]
+            lastLng = geo["lng"]
+            gpx = ET.Element("gpx", version="1.1", creator="Xcode")
+            wpt = ET.SubElement(gpx, "wpt", lat=geo["lat"], lon=geo["lng"])
+            ET.SubElement(wpt, "name").text = "PokemonLocation"
+            ET.ElementTree(gpx).write("pokemonLocation.gpx")
+            print "Location Updated!", "latitude:", geo["lat"], "longitude:" ,geo["lng"]
+            clickAction()
 
 def start():
     while True:
